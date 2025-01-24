41df76288195923eb95100a03a7dd392740e327d
istvzsig <istvanzsigmond@aol.com>
2025-01-24 14:12:42 +0100

    update package

--- index.js
+++ index.js
@@ -1,41 +1,44 @@
-// cursorHelper.js
+// index.js
 
-const { MongoClient } = require('mongodb');
+import { MongoClient } from 'mongodb';
 
 class CursorHelper {
     constructor(dbUrl, dbName) {
-        this.client = new MongoClient(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+        this.dbUrl = dbUrl;
         this.dbName = dbName;
+        this.client = new MongoClient(this.dbUrl);
+        this.db = null;
     }
 
     async connect() {
-        await this.client.connect();
-        this.db = this.client.db(this.dbName);
+        try {
+            await this.client.connect();
+            this.db = this.client.db(this.dbName);
+            console.log('Connected to database:', this.dbName);
+        } catch (error) {
+            console.error('Error connecting to the database:', error);
+            throw error;
+        }
     }
 
-    async getCursor(collectionName, query = {}, options = {}) {
-        const collection = this.db.collection(collectionName);
-        return collection.find(query, options);
-    }
-
-    async paginate(collectionName, query = {}, options = {}, pageSize = 10, page = 1) {
-        const cursor = await this.getCursor(collectionName, query, options);
-        const totalCount = await cursor.count();
-        const totalPages = Math.ceil(totalCount / pageSize);
-        const results = await cursor.skip((page - 1) * pageSize).limit(pageSize).toArray();
+    async paginate(collectionName, query, options, limit, page) {
+        const skip = (page - 1) * limit;
+        const cursor = this.db.collection(collectionName).find(query, options).skip(skip).limit(limit);
+        const results = await cursor.toArray();
+        const totalCount = await this.db.collection(collectionName).countDocuments(query);
 
         return {
-            page,
-            pageSize,
-            totalCount,
-            totalPages,
             results,
+            totalCount,
+            totalPages: Math.ceil(totalCount / limit),
+            currentPage: page,
         };
     }
 
     async close() {
         await this.client.close();
+        console.log('Connection closed');
     }
 }
 
-module.exports = CursorHelper;
+export default CursorHelper;
